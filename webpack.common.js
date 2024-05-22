997639dbc4b08d8a99df0f27b22c225d3e788b8a
SoTilted <103329657+SoTilted@users.noreply.github.com>
2024-05-22 11:26:11 +0300

    Configure weback to have multiple htmlpages

--- webpack.common.js
+++ webpack.common.js
@@ -2,42 +2,51 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-      index: "./src/index.js"
-    },
-    output: {
-      filename: '[name].bundle.js',
-        path: path.resolve(__dirname, "dist"),
-        assetModuleFilename: 'assets/',
-        clean: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        inject: 'body'
-      })
+  entry: {
+    index: "./src/index.js",
+    form: "./src/form.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+      chunks: ["index"],
+    }),
+    new HtmlWebpackPlugin({
+      // Also generate a form.html
+      template: "./src/form.html",
+      filename: "form.html",
+      inject: "body",
+      chunks: ["form"],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[hash][ext][query]",
+        },
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-              test: /\.(png|svg|jpg|jpeg|gif)$/i,
-              type: 'asset/resource',
-              generator: {
-                filename: 'images/[hash][ext][query]'
-              }
-            },
-            {
-              test: /\.(woff|woff2|eot|ttf|otf)$/i,
-              type: 'asset/resource',
-              generator: {
-                filename: 'fonts/[hash][ext][query]'
-              }
-            },
-        ],
-    },
+  },
 };
