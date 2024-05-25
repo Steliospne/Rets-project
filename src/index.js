ba9cd793a98783bf344e0c5a305de9bb9bad5b03
Stelios <63586679+Steliospne@users.noreply.github.com>
2024-05-25 16:06:59 +0200

    Cleaning code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 import "./style.css";
 import disclaimerWarning from "./disclaimer";
 import Spinner from "./spinner.js";
-disclaimerWarning();
-Spinner.generate();
+import backDisabler from "./backDisabler.js";
+
+const dialog = document.querySelector("dialog");
+const dialogCancelButton = document.querySelector("#cancel");
+const dialogContinueButton = document.querySelector("#confirmBtn");
 const code = window.location.href.slice(window.location.href.indexOf("?"));
-window.onbeforeunload = confirmExit;
+
 function confirmExit() {
   if (dialog.value !== "end") {
     return "You have attempted to leave this page. Are you sure?";
   }
 }
-const dialog = document.querySelector("dialog");
-const dialogCancelButton = document.querySelector("#cancel");
-const dialogContinueButton = document.querySelector("#confirmBtn");
-dialogCancelButton.addEventListener("click", () => {
-  dialog.close();
-});
-
-dialogContinueButton.addEventListener("click", doStuff);
 
 function doStuff() {
   dialog.close();
   if (dialog.value === "Spin Again") return;
   dialogContinueButton.parentElement.href = `http://localhost:8080/form${code}`;
 }
+
+dialogCancelButton.addEventListener("click", () => {
+  dialog.close();
+});
+
+dialogContinueButton.addEventListener("click", doStuff);
+window.onbeforeunload = confirmExit;
+backDisabler();
+disclaimerWarning();
+Spinner.generate();
