f23f14d06f6572b4b23f936927c93f0ea06c4a20
Stelios P. <63586679+Steliospne@users.noreply.github.com>
2024-05-30 22:23:36 +0200

    Change in how the url is passed to the server

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import backDisabler from "./backDisabler.js";
 const dialog = document.querySelector("dialog");
 const dialogCancelButton = document.querySelector("#cancel");
 const dialogContinueButton = document.querySelector("#confirmBtn");
-const code = window.location.href.slice(window.location.href.indexOf("?"));
 
-const baseURL = "http://donuts.rets.gr/";
-const baseURLdev = "http://localhost:8080/";
+const baseURL = window.location.href.slice(0,window.location.href.indexOf("?"));
+const code = window.location.href.slice(window.location.href.indexOf("?"));
 
 function confirmExit() {
   if (dialog.value !== "end") {
@@ -30,5 +29,5 @@ dialogCancelButton.addEventListener("click", () => {
 dialogContinueButton.addEventListener("click", doStuff);
 window.onbeforeunload = confirmExit;
 backDisabler();
-disclaimerWarning();
 Spinner.generate();
+disclaimerWarning();
